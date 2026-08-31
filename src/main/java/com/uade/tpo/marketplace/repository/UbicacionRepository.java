3feb27b08d9021179e38129488eb21032376976a
Lucio <lclaa@uade.edu.ar>
2026-08-31 15:52:14 -0300

    Cambios en ubicacion y metodo update

--- src/main/java/com/uade/tpo/marketplace/repository/UbicacionRepository.java
+++ src/main/java/com/uade/tpo/marketplace/repository/UbicacionRepository.java
@@ -1,7 +1,9 @@
 package com.uade.tpo.marketplace.repository;
 
+import com.uade.tpo.marketplace.entity.Ubicacion;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UbicacionRepository {
+public interface UbicacionRepository extends JpaRepository<Ubicacion, Long> {
 }
--- src/main/java/com/uade/tpo/marketplace/service/UbicacionServiceImpl.java
+++ src/main/java/com/uade/tpo/marketplace/service/UbicacionServiceImpl.java
@@ -1,7 +1,29 @@
 package com.uade.tpo.marketplace.service;
 
+import com.uade.tpo.marketplace.entity.Ubicacion;
+import com.uade.tpo.marketplace.repository.UbicacionRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UbicacionServiceImpl {
+public class UbicacionServiceImpl implements UbicacionService {
+
+    @Autowired
+    private UbicacionRepository ubicacionRepository;
+
+    @Override
+    public Ubicacion updateUbicacion(Long idUbicacion, Ubicacion nuevosDatos) {
+
+        Ubicacion ubicacionExistente = ubicacionRepository.findById(idUbicacion)
+                .orElseThrow(() -> new EntityNotFoundException("Ubicación no encontrada"));
+
+        ubicacionExistente.setDireccion(nuevosDatos.getDireccion());
+        ubicacionExistente.setCiudad(nuevosDatos.getCiudad());
+        ubicacionExistente.setProvincia(nuevosDatos.getProvincia());
+        ubicacionExistente.setCodigoPostal(nuevosDatos.getCodigoPostal());
+        ubicacionExistente.setLocalidad(nuevosDatos.getLocalidad());
+
+        return ubicacionRepository.save(ubicacionExistente);
+    }
 }
