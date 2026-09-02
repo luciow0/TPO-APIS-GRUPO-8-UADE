1ef1445a42b9ba639e5b5ecb53d11793be9d5e47
AlexUADE <aclaros@uade.edu.ar>
2026-09-01 22:02:10 -0300

    adaptacion nombre de metodo en publicacion

--- src/main/java/com/uade/tpo/marketplace/service/PublicacionServiceImpl.java
+++ src/main/java/com/uade/tpo/marketplace/service/PublicacionServiceImpl.java
@@ -38,7 +38,7 @@ public class PublicacionServiceImpl implements PublicacionService {
                 validarRequest(request);
 
                 Vehiculo vehiculo = vehiculoService
-                                .obtenerVehiculoPorId(request.getIdVehiculo())
+                                .buscarPorId(request.getIdVehiculo())
                                 .orElseThrow(() -> new ResponseStatusException(
                                                 HttpStatus.NOT_FOUND,
                                                 "Vehiculo no encontrado"));
@@ -98,7 +98,7 @@ public class PublicacionServiceImpl implements PublicacionService {
                 validarRequest(request);
 
                 Vehiculo vehiculo = vehiculoService
-                                .obtenerVehiculoPorId(request.getIdVehiculo())
+                                .buscarPorId(request.getIdVehiculo())
                                 .orElseThrow(() -> new ResponseStatusException(
                                                 HttpStatus.NOT_FOUND,
                                                 "Vehiculo no encontrado"));
