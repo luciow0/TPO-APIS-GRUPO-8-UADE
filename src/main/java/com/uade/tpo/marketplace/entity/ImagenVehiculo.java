5ab7ed1eb572fd337ecfb20114628946b734701e
bautista00 <bautistaquesada00@gmail.com>
2026-09-06 16:44:58 -0300

    Solucion de recursion infinita de serializacion

--- src/main/java/com/uade/tpo/marketplace/entity/ImagenVehiculo.java
+++ src/main/java/com/uade/tpo/marketplace/entity/ImagenVehiculo.java
@@ -2,6 +2,7 @@ package com.uade.tpo.marketplace.entity;
 
 import java.sql.Blob;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -32,6 +33,7 @@ public class ImagenVehiculo {
 
     // Imagen guardada como binario (Blob) en la BD
     // Nunca se serializa directo: se devuelve en base64 vía ImagenVehiculoResponse.
+   @JsonIgnore
     private Blob imagen;
 
     @NotNull
@@ -40,6 +42,7 @@ public class ImagenVehiculo {
     @Column(nullable = false)
     private Integer orden;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "id_vehiculo", nullable = false)
     private Vehiculo vehiculo;
